25b66b3d2f55862cb8b2d0a969ba66af51f1c0a9
Dipak Niroula <sangit.niroula@gmail.com>
2024-09-09 23:58:03 +0545

    validation logic and utility functions added

--- logs/log_gen.py
+++ logs/log_gen.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 
 BASE_FOLDERS = ("folder_1", "folder_2", "folder_3")
-BASE_YEARS = ("2022", "2023", "2024")
-BASE_MONTHS = [str(MONTH) for MONTH in range(1, 13)]
-BASE_DAYS = [str(DAY) for DAY in range(1, 26)]
+BASE_YEARS = ("2024", )
+BASE_MONTHS = ("09", )
+BASE_DAYS = [str(DAY).zfill(2) for DAY in range(1, 33)]
 
 
 def generate_log_files():
@@ -25,14 +25,18 @@ def generate_log_files():
                     base_day_dir.mkdir(parents=False, exist_ok=True)
                     date_str = f"{year}-{month}-{day} 09:00:00"
                     date_format = '%Y-%m-%d %H:%M:%S'
-                    date_time_obj = datetime.strptime(date_str, date_format)
+                    try:
+                        date_time_obj = datetime.strptime(date_str, date_format)
+                    except:
+                        continue
                     timestamp = int(date_time_obj.timestamp())
                     for _ in range(20):
                         file_name = f"{folder}/{year}/{month}/{day}/{timestamp}_file"
                         with open(file_name, "w") as file:
-                            content = f"File name is {file_name} and the start timestamp is {timestamp}"
-                            file.write(content)
-                        timestamp += timestamp + 600
+                            for i in range(100000):
+                                content = f"File name is {file_name} and the start timestamp is {timestamp} and i is {i}\n"
+                                file.write(content)
+                        timestamp += 600
         print(f"Folder: {folder} is ready")
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::path::Path;
 use clap::Parser;
-
-
+use chrono::{NaiveDate, Duration};
 
 #[derive(Parser, Debug)]
 #[command(name="Log Exporter", about="To export raw logs to S3")]
@@ -15,22 +15,82 @@ struct CLI {
 
 
 
+fn get_date_ranges(start_date: &str, end_date: &str) -> Vec<String> {
+    let start_date = NaiveDate::parse_from_str(start_date, "%Y-%m-%d").unwrap();
+    let end_date = NaiveDate::parse_from_str(end_date, "%Y-%m-%d").unwrap();
+
+    let mut current_date = start_date;
+    let mut date_list = Vec::new();
+
+    while current_date <= end_date {
+        date_list.push(current_date.to_string());
+        current_date = current_date + Duration::days(1);
+    }
+    date_list
+}
+
+
+fn get_full_path(folder_name: &str, date: &str) -> String {
+    let path_vec: Vec<&str> = date.split("-").collect();
+    let path = path_vec.join("/");
+    let full_path = format!("{}/{}/{}", "logs", folder_name, path);
+    full_path
+}
+
+
+fn export_files(folder_name: &str, start_date: &str, end_date: &str) {
+    let all_possible_folders = get_date_ranges(start_date, end_date);
+    println!("{:?}", all_possible_folders);
+    for folder in all_possible_folders.iter() {
+        let path = get_full_path(folder_name, folder);
+        if Path::new(&path).exists() {
+            println!("Path exists: {}", path);
+        } else {
+            println!("Path does not exists: {}", path);
+        }
+    }
+
+}
+
+
+fn validate_supplied_date(start_date: &str, end_date: &str) -> Result<String, String> {
+    match NaiveDate::parse_from_str(start_date, "%Y-%m-%d") {
+        Ok(start_date) => {
+            match NaiveDate::parse_from_str(end_date, "%Y-%m-%d") {
+                Ok(end_date) => {
+                    if end_date < start_date {
+                        Err("End date is smaller than start date".to_string())
+                    } else {
+                        Ok("Valid date range".to_string())
+                    }
+                },
+                Err(e) => Err("Invalid end date".to_string()),
+            }
+        },
+        Err(e) => Err("Invalid start date".to_string()),
+    }
+}
+
 fn main() {
     println!("Starting!\n-----------------------\n");
 
     let args = CLI::parse();
 
     let folder_names = args.folder_names;
-    let folders: Vec<String> = folder_names.split(",").map(|s| s.trim().to_string()).collect();
+
     let start_date = args.start_date;
     let end_date = args.end_date;
 
-
-    for folder in folders.iter() {
-        println!("Exporting logs from folder: {}", folder);
-        println!("Start date: {:?}", start_date);
-        println!("End date: {:?}", end_date);
+    match validate_supplied_date(&start_date, &end_date) {
+        Ok(msg) => {
+            let folders: Vec<String> = folder_names.split(",").map(|s| s.trim().to_string()).collect();
+            for folder in folders.iter() {
+                export_files(&folder, &start_date, &end_date);
+            }
+        },
+        Err(e) => println!("Error: {}", e)
     }
+
 
 
     println!("\n------------------------\nFinished!");
